f75639bcd1411158753a7f93d8af7a65d3e75b06
WilsonTomisin <tommywilson972@gmail.com>
2025-08-24 18:50:22 +0100

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,11 +14,19 @@ function App() {
       </section>
     )
   }
+
   return (
-    <>
-    <RouterProvider router={MyRouter}/>
-    </>
+    <section className=" h-screen flex items-center justify-center">
+      <div className=" flex items-center gap-2 text-red-500">
+        <IoMdInformationCircleOutline /> Not available.
+      </div>
+    </section>
   )
+  // return (
+  //   <>
+  //   <RouterProvider router={MyRouter}/>
+  //   </>
+  // )
 }
 
 export default App
